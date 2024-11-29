96603f882fd930718545fcdafab0221611c60582
Lauvrar <00001132176402sp@al.educacao.sp.gov.br>
2024-11-28 23:56:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Geografia',
+    'Qual o maior deserto do mundo?',
+    'O deserto de Antártida'
+)
+
+criaCartao(
+    'Arte',
+    'O que é movimento Impressionista?',
+    'Um estilo do final do século XIX, com foco nos efeitos de luz e variedades de cores'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula da área de um triângulo?',
+    '(base x altura) % 2'
+)
+
